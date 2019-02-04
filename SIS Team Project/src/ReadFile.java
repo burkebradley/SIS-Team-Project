84880d1af3579709bb6fd9baafbac670e405009e
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2019-02-04 09:09:55 -0700

    started file reading

---
+++ SIS Team Project/src/ReadFile.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class ReadFile
+	{
+
+		public static void readFile()
+			{
+				
+			String line = null;	
+			try
+			{
+				
+			BufferedReader br = new BufferedReader(new FileReader("StudentList.txt"));
+			int counter=0;
+			int counter2=2;
+			int counter3=0;
+			
+			while((line = br.readLine()) != null) 
+		 	{
+		    String[] student = line.split(" ");
+		    Runner.directory.add(new Student((student[0]+" "+student[1]), 0, new ArrayList<Course>()));
+		    
+		    for(int i=0; i<3; i++)
+		    	{
+		    	
+		    		Runner.directory.get(counter).getClasses().add(new Course(student[counter2], student[counter2+1], counter3+1));
+		    		counter2++;
+		    		counter3++;
+		    		
+		    	}
+		    counter++;
+		    counter2=2;
+		    counter3=0;
+		 	}
+			
+			br.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println("bet");
+			}
+			}
+		
+	}
--- SIS Team Project/src/Runner.java
+++ SIS Team Project/src/Runner.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
 
 public class Runner
 	{
 
+		static ArrayList<Student> directory = new ArrayList<Student>();
+		
 		public static void main(String[] args)
 			{
+				ReadFile.readFile();
 				
+				for(Student s: directory)
+					{
+						System.out.println(s.getName()+": "+s.getGpa()+", "+s.getClasses().get(0).getName()+": "+s.getClasses().get(0).getGrade()+", "+s.getClasses().get(0).getPeriod()+", "+s.getClasses().get(1).getName()+": "+s.getClasses().get(1).getGrade()+", "+s.getClasses().get(1).getPeriod()+s.getClasses().get(2).getName()+": "+s.getClasses().get(2).getGrade()+", "+s.getClasses().get(2).getPeriod());
+					}
 			}
 		
 	}
