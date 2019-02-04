c281ffaf52040e61ac7dea0305172968fd760e41
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2019-02-04 08:45:58 -0700

    created objects and comparators

---
+++ SIS Team Project/src/Course.java
@@ -0,0 +1,47 @@
+
+public class Course
+	{
+		private String name;
+		private String grade;
+		private int period;
+		
+		public Course(String n, String g, int p)
+		{
+			name = n ;
+			grade = g;
+			period = p;
+		}
+
+		public int getPeriod()
+			{
+				return period;
+			}
+
+		public void setPeriod(int period)
+			{
+				this.period = period;
+			}
+
+		public String getName()
+			{
+				return name;
+			}
+
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+
+		public String getGrade()
+			{
+				return grade;
+			}
+
+		public void setGrade(String grade)
+			{
+				this.grade = grade;
+			}
+		
+		
+
+	}
